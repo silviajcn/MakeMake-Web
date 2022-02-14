b51088c0dacdd571f86d17b90d9b4041a0d37c7a
Silvia <silviajcn@gmail.com>
2022-02-14 15:19:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import './styles/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRouter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppRouter = () => {
+    return (
+        <div>
+            <h1>MakeMake</h1>
+        </div>
+    )
+};
+
+export default AppRouter;
